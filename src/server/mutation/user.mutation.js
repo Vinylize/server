97a8dff281c53c6bf726f437ac2e532498e6b2bd
Yangwoo Lee <lyw0149@gmail.com>
2017-01-27 18:43:24 +0900

    Add updateCoordinate mutation.

--- src/server/mutation/user.mutation.js
+++ src/server/mutation/user.mutation.js
@@ -1,4 +1,5 @@
 import {
+  GraphQLFloat,
   GraphQLString,
   GraphQLNonNull
 } from 'graphql';
@@ -8,51 +9,80 @@ import {
 
 import firebase from '../util/firebase.util';
 
-const UserMutation = {
-  createUser: mutationWithClientMutationId({
-    name: 'createUser',
-    inputFields: {
-      email: {type: new GraphQLNonNull(GraphQLString)},
-      name: {type: new GraphQLNonNull(GraphQLString)},
-      password: {type: new GraphQLNonNull(GraphQLString)}
-    },
-    outputFields: {
-      result: {
-        type: GraphQLString,
-        resolve: (payload) => payload.result
-      }
-    },
-    mutateAndGetPayload: ({email, password, name}) => {
-      return new Promise((resolve, reject) => {
-        firebase.admin.auth().createUser({
-          email: email,
-          emailVerified: false,
-          password: password,
-          displayName: name,
-          disabled: false
-        })
-          .then((createdUser) => {
-            return firebase.refs.user.child(createdUser.uid).set({
-              ...firebase.defaultSchema.user
-            })
-              .then(() => {
-                return firebase.refs.userPortQualification.child(createdUser.uid).set({
-                  ...firebase.defaultSchema.userPortQualification
-                });
-              })
-              .then(() => {
-                return firebase.refs.userShipQualification.child(createdUser.uid).set({
-                  ...firebase.defaultSchema.userShipQualification
-                });
-              });
+const createUserMutation = {
+  name: 'createUser',
+  inputFields: {
+    email: {type: new GraphQLNonNull(GraphQLString)},
+    name: {type: new GraphQLNonNull(GraphQLString)},
+    password: {type: new GraphQLNonNull(GraphQLString)}
+  },
+  outputFields: {
+    result: {
+      type: GraphQLString,
+      resolve: (payload) => payload.result
+    }
+  },
+  mutateAndGetPayload: ({email, password, name}) => {
+    return new Promise((resolve, reject) => {
+      firebase.admin.auth().createUser({
+        email: email,
+        emailVerified: false,
+        password: password,
+        displayName: name,
+        disabled: false
+      })
+        .then((createdUser) => {
+          return firebase.refs.user.child(createdUser.uid).set({
+            ...firebase.defaultSchema.user
           })
+            .then(() => {
+              return firebase.refs.userPortQualification.child(createdUser.uid).set({
+                ...firebase.defaultSchema.userPortQualification
+              });
+            })
+            .then(() => {
+              return firebase.refs.userShipQualification.child(createdUser.uid).set({
+                ...firebase.defaultSchema.userShipQualification
+              });
+            });
+        })
+        .then(() => {
+          resolve({result: 'OK'});
+        })
+        .catch(reject);
+    });
+  }
+};
+
+const updateCoordinateMutation = {
+  name: 'updateCoordinate',
+  inputFields: {
+    lat: {type: new GraphQLNonNull(GraphQLFloat)},
+    lon: {type: new GraphQLNonNull(GraphQLFloat)}
+  },
+  outputFields: {
+    result: {
+      type: GraphQLString,
+      resolve: (payload) => payload.result
+    }
+  },
+  mutateAndGetPayload: (args, { user }) => {
+    return new Promise((resolve, reject) => {
+      if (user) {
+        return firebase.refs.userCoordinate.child(user.uid).set(args)
           .then(() => {
             resolve({result: 'OK'});
           })
           .catch(reject);
-      });
-    }
-  })
+      }
+      return reject('This mutation needs accessToken.');
+    });
+  }
+};
+
+const UserMutation = {
+  createUser: mutationWithClientMutationId(createUserMutation),
+  updateCoordinate: mutationWithClientMutationId(updateCoordinateMutation)
 };
 
 export default UserMutation;
